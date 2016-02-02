6a4f5e8c3169d7f398685ddd827406a44b9f33ee
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-02-02 20:27:08 +0300

    lowercase trigger word

--- bot.rb
+++ bot.rb
@@ -10,12 +10,13 @@ end
 post '/gateway' do
   return {text: 'Don\'t talk to strangers'} if params[:token] != ENV['SLACK_TOKEN']
   client = MongoClient.new(params)
+  trigger = params[:trigger_word].downcase
 
-  if params[:trigger_word] == 'standup:'
+  if trigger == 'standup:'
     client.remove_previous_standups
     client.insert_recent_standup
     respond_with CheersUp.greet
-  elsif params[:trigger_word] == 'standup?'
+  elsif trigger == 'standup?'
     respond_with(text: client.resent_standups)
   end
 end
